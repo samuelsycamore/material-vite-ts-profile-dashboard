67fed9d81f3486235657fa778b92a583c0838aaa
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-08-01 19:49:11 -0500

    boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { CssBaseline } from "@mui/material";
 import theme from "./theme.tsx";
 import App from "./App";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
