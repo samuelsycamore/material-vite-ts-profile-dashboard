8947ee5e6c5f03da9e36be10004fb7dcd3fae340
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-10-01 16:23:31 -0500

    wonky but functional mode toggle

--- src/components/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle.tsx
@@ -1,7 +1,31 @@
-export default function ColorSchemeToggle() {
+import { Box, IconButton } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useThemeContext } from "../theme/ThemeContextProvider";
+
+const ColorSchemeToggle = () => {
+  const { mode, toggleColorMode } = useThemeContext();
+
   return (
-    <>
-      <span>thing</span>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderColor: "text.primary",
+        border: "1px solid",
+        borderRadius: 25,
+        p: 2,
+      }}
+    >
+      {mode} mode
+      <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+        {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+    </Box>
   );
-}
+};
+
+export default ColorSchemeToggle;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import theme from "./theme.tsx";
+import theme from "./theme/theme.tsx";
 import App from "./App";
+import { ThemeContextProvider } from "./theme/ThemeContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <ThemeContextProvider>
       <App />
-    </ThemeProvider>
+    </ThemeContextProvider>
   </React.StrictMode>,
 );
