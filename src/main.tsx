4d3985b5c63530b588d5d3e5ed64369c9cfb2f9b
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-10-01 16:36:35 -0500

    fix more ts errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
-import theme from "./theme/theme.tsx";
 import App from "./App";
 import { ThemeContextProvider } from "./theme/ThemeContextProvider.tsx";
 
